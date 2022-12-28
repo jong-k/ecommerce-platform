b26339e9ec7322fcbb1ce03f5ca9d94eeb7a6f1e
김종한 <kjhan710@naver.com>
2022-12-28 22:04:04 +0900

    Delete textbook directory

--- textbook/ch02-JS-for-React/fetch.js
+++
@@ -1,5 +0,0 @@
-fetch("https://randomuser.me/api/?nat=us&results=1") // GET 요청 보냄
-  .then((res) => res.json()) // 요청 성공하면 JSON 파일을 JS 객체로 변환
-  .then((json) => json.results) // 객체에서 results 프로퍼티만 꺼냄
-  .then(console.log) // resolve 시 콘솔에 출력
-  .catch(console.error); // reject 시 콘솔에 에러 출력
