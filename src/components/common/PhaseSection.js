b9068c8e7b1346fe9f552159e498b76246eae909
jong-k <kjhan710@naver.com>
2023-02-22 11:46:15 +0900

    initial commit

--- src/components/common/PhaseSection.js
+++
@@ -1,9 +0,0 @@
-const PhaseSection = ({ header, category }) => {
-  return (
-    <main className="status-section">
-      <h2 className="section-header">{header}</h2>
-    </main>
-  );
-};
-
-export default PhaseSection;
--- src/utils/jsonLocalStorage.js
+++
@@ -1,7 +0,0 @@
-export const addItem = (key, val) => {
-  localStorage.setItem(key, JSON.stringify(val));
-};
-
-export const readItem = (key) => {
-  return JSON.parse(localStorage.getItem(key));
-};
