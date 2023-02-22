567ba5a23adc6715e34def0f177a0d258e763c8c
jong-k <kjhan710@naver.com>
2023-02-22 13:06:42 +0900

    feat: 기반 파일 추가

---
+++ src/components/IssueList.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+const IssueList = () => {
+  return <div></div>;
+};
+
+export default IssueList;
---
+++ src/enums/index.ts
@@ -0,0 +1,32 @@
+export const ISSUE_STATE = {
+  TODO: { value: "todo", label: "할 일" },
+  IN_PROGRESS: { value: "progress", label: "진행중" },
+  COMPLETE: { value: "complete", label: "완료" },
+};
+
+export const ISSUE_MODAL_TITLE = {
+  ADD: "등록",
+  EDIT: "수정",
+};
+
+export const ISSUE_FORM_NAME = {
+  TITLE: "title",
+  MANAGER: "manager",
+  DESCRIPTION: "description",
+  LAST_DATE: "lastDate",
+};
+
+export const ISSUE_FORM_LABEL = {
+  TITLE: "제목",
+  MANAGER: "담당자",
+  DESCRIPTION: "내용",
+  STATE: "상태",
+  LAST_DATE: "마감일",
+};
+
+export const ISSUE_FORM_PLACEHOLDER = {
+  TITLE: "제목을 입력해주세요",
+  MANAGER: "담당자를 선택해주세요",
+  DESCRIPTION: "내용을 입력해주세요",
+  LAST_DATE: "마감일을 선택해주세요",
+};
